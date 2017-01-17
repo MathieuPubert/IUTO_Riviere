fe5af081e65c351e72c93c28defa70071fae27fe
tortueC <tortuedeschamps@hotmail.fr>
2017-01-17 12:46:31 +0100

    Update ia.py

--- versionClassique/ia.py
+++ versionClassique/ia.py
@@ -5,7 +5,18 @@ from jeu import *
 # Cette fonction retourne une grille hexgonale qui donne pour chaque case de la
 # rivière sa distance jusqu'à la case qui se trouve en lig,col
 def marquage(riviere,lig,col):
-    pass
+    marque=GrilleHexa(getNbLigR(riviere),getNbColR(riviere),estPaireR(riviere),0)
+    for cpt in range(int(getNbLigR(riviere)*(getNbColR(riviere)/2))):
+        ligCpt=1
+        if estPaireR(riviere) :
+            colCpt=1
+        else :
+            ligCpt=2
+        marque["valeurs"][ligCpt][colCpt]=abs(lig-ligCpt+col-colCpt)
+        if colCpt+2>getNbColR(riviere):
+            ligCpt+=1
+        else:
+            colCpt+=1
 
 # fonction qui choisit automatiquement une direction (qui doit être une 
 # direction valide) pour le joueur courant. L'implémentation choisi aléatoirement
