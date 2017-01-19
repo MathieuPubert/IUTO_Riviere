b01e95417b62829fcb130ab5c308a957aeef7c1c
tortueC <tortuedeschamps@hotmail.fr>
2017-01-19 09:15:49 +0100

    Update ia.py

--- versionClassique/ia.py
+++ versionClassique/ia.py
@@ -8,15 +8,16 @@ from jeu import *
 def marquage(riviere, lig, col):
     marque = GrilleHexa(getNbLigR(riviere), getNbColR(riviere), estPaireR(riviere), 0)
     i=0
-    while i < len(marque["valeurs"]):
+    while i < getNbLigGH(marque):
         j=0
-        while j < len(marque["valeurs"][i]):
+        while j < getNbColGH(marque):
             if estPosGH(marque, i, j):
                 (a,b)=(i,j)
                 (a,b)=((a+b)/2,(b-a)/2)
                 (c,d)=(lig,col)
                 (c,d)=((c+d)/2,(d-c)/2)
-                marque["valeurs"][i][j] = int((abs(c-a)+abs(d-b)+abs(c-a+d-b))/2)
+                res = int((abs(c-a)+abs(d-b)+abs(c-a+d-b))/2)
+                setValGH(marque, i, j, res)
             j+=1
         i+=1
     return marque
@@ -30,16 +31,12 @@ def choixDirection(jeu):
     marque = marquage(r, getNbLigR(r), getColArrivee(r))
     (lig, col) = getPosJoueurCourant(jeu)
     listeDirectionVerif = []
-    direction = {"S": marque["valeurs"][lig + 2][col], "SO": marque["valeurs"][lig + 1][col - 1],
-                 "SE": marque["valeurs"][lig + 1][col + 1], "N": marque["valeurs"][lig - 2][col],
-                 "NE": marque["valeurs"][lig - 1][col + 1], "NO": marque["valeurs"][lig - 1][col - 1]}
+    direction = {"S": getValGH(marque, lig+2, col), "SO": getValGH(marque, lig+1, col-1),
+                 "SE": getValGH(marque, lig+1, col+1), "N": getValGH(marque, lig-2, col),
+                 "NE": getValGH(marque, lig-1, col+1), "NO": getValGH(marque, lig-1, col-1)}
     for elem in direction:
-        print ("testDebug getPosJoueurCourant :")
-        print (getPosJoueurCourant(game))
         if verifDirection(jeu, elem) == DIRECTION_OK:
             listeDirectionVerif.append(direction[elem])
-    print ("testDebug listeDirectionVerif :")
-    print (listeDirectionVerif)
     for a, b in direction.items():
         if b == min(listeDirectionVerif):
             return a
@@ -57,15 +54,14 @@ def choixDirectionAlea(jeu):
 
 # TESTS
 if __name__ == '__main__':
+
     print ("Test marquage(Riviere(12, 9, True, 0, 0),6,4) :")
     afficheGH(marquage(Riviere(12, 9, True, 0, 0),6,4))
+    print ("\n")
 
     game=Jeu('./data/', 'joueurs.txt', 'riviere1.txt')
-    initJeu(game)
     for (nom, representation) in getJoueursPossibles(getJoueursJ(game)).items():
-        ajouterJoueurJ(game, Joueur(nom, representation))
+        ajouterJoueurJ(game, Joueur(nom, representation,True))
+    initJeu(game)
 
-    print ("Test choixDirection(game) :")
-    choixDirection(game)
-    print ("Test choixDirectionAlea(game) :")
-    choixDirectionAlea(game)
+    print ("Test choixDirection(game) :", choixDirection(game))
